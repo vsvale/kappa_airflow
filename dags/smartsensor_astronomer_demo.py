2c5fff0b56a628b669e0a473b4fc70fe45a14340
dataeng <viniciusdvale@gmail.com>
2022-09-25 12:11:10 -0300

    smartsensor astronomer demo

---
+++ dags/smartsensor_astronomer_demo.py
@@ -0,0 +1,36 @@
+from airflow.decorators import dag, task
+from airflow.utils.dates import days_ago
+from airflow.hooks.filesystem import FSHook
+from plugins.smart_file_sensor import SmartFileSensor
+
+import logging
+import json
+
+default_args = {
+    'start_date': days_ago(1),
+}
+
+@dag(schedule_interval='@daily', default_args=default_args, catchup=False)
+def smartsensor_astronomer_demo():
+    FILENAME = 'bitcoin.json'
+
+    sensors = [
+        SmartFileSensor(
+            task_id=f'waiting_for_file_{sensor_id}',
+            filepath=FILENAME,
+            fs_conn_id='fs_default'
+        ) for sensor_id in range(1, 10)
+    ]
+
+    @task
+    def proccessing():
+        path = FSHook(conn_id='fs_default').get_path()
+        logging.info(f'{path}/{FILENAME}')
+
+    @task
+    def storing():
+        logging.info('storing data')
+
+    sensors >> proccessing() >> storing()
+
+dag = smartsensor_astronomer_demo()
