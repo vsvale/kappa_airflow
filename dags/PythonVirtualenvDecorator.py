864922b4cf2bf3f43f9d02652efc6129d80b6d00
dataeng <viniciusdvale@gmail.com>
2022-09-25 14:37:17 -0300

    python vitural end decorator

---
+++ dags/PythonVirtualenvDecorator.py
@@ -0,0 +1,24 @@
+from airflow.decorators import dag, task
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'start_date': days_ago(1),
+}
+
+@dag(schedule_interval='@daily', default_args=default_args, catchup=False)
+def pythonvirtualenvdecorator():
+
+    @task.virtualenv(
+        use_dill=True,
+        system_site_packages=False,
+        requirements=[
+            'dill==0.3.4'
+            ],
+    )
+    def extract():
+        import json # You must make your imports within the function
+        data_string = '{"1001": 301.27, "1002": 433.21, "1003": 502.22}'
+        order_data_dict = json.loads(data_string)
+        return order_data_dict
+    extract()
+dag = pythonvirtualenvdecorator()
