a04733dc408bac7b09cfb76ee792c88e8d6b56dc
dataeng <viniciusdvale@gmail.com>
2022-10-03 16:29:31 -0300

    decorator not works

--- dags/astronomer_calendar_branch.py
+++ dags/astronomer_calendar_branch.py
@@ -1,5 +1,6 @@
 from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
+from airflow.operators.python import BranchPythonOperator
 from airflow.operators.empty import EmptyOperator
 import yaml
 
@@ -7,20 +8,19 @@ default_args = {
     'start_date': days_ago(1),
 }
 
+def _is_holiday(ds):
+    with open('include/holidays.yaml','r') as f:
+        doc = yaml.load(f,Loader=yaml.SafeLoader)
+        if (ds in doc['holidays']):
+            return 'dont_process'
+        return 'start'
+
 
 @dag(schedule_interval='@daily', default_args=default_args, catchup=False, tags=['astronomer', 'branch','calendar'])
 def astronomer_calendar_branch():
-    ds = context.get("execution_date")
-    @task.branch()
-    def is_holliday(ds):
-        with open('include/holidays.yaml','r') as f:
-            doc = yaml.load(f,Loader=yaml.SafeLoader)
-            if (ds in doc['holidays']):
-                return 'dont_process'
-            return 'start'
-
+    is_holiday = BranchPythonOperator(task_id='is_holiday',python_callable=_is_holiday)
     start = EmptyOperator(task_id='start')
     dont_process = EmptyOperator(task_id='dont_process')
 
-    is_holliday(ds)>>[start,dont_process]
+    is_holiday>>[start,dont_process]
 dag = astronomer_calendar_branch()
