931892c3d8d9e308898305edad8d44088afa8408
dataeng <viniciusdvale@gmail.com>
2022-10-03 15:05:31 -0300

    old way xcom

--- dags/astronomer_branchpythonoperator.py
+++ dags/astronomer_branchpythonoperator.py
@@ -1,14 +1,17 @@
 from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
 from airflow.operators.dummy import DummyOperator
-from airflow.operators.python import BranchPythonOperator
+from airflow.operators.python import BranchPythonOperator, PythonOperator
 
 default_args = {
     'start_date': days_ago(1),
 }
 
-def _check_accuracy():
-    accuracy = 0.7
+def _training_model():
+    return 0.7
+
+def _check_accuracy(ti):
+    accuracy = ti.xcom_pull(task_id='training_model')
     if (accuracy < 0.5):
         return ['is_inaccurate','send_slack']
     return 'is_accurate'
@@ -16,7 +19,7 @@ def _check_accuracy():
 @dag(schedule_interval='@daily', default_args=default_args, catchup=False, tags=['astronomer', 'branch'])
 def astronomer_branchpythonoperator():
 
-    training_model = DummyOperator(task_id='training_model')
+    training_model = DummyOperator(task_id='training_model',python_callable=_training_model)
     check_acc = BranchPythonOperator(task_id='check_accuracy', python_callable=_check_accuracy,do_xcom_push=False)
     is_accurate = DummyOperator(task_id='is_accurate')
     is_inaccurate = DummyOperator(task_id='is_inaccurate')
