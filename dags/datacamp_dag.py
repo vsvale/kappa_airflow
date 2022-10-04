231d3787233a374a6ccbd37c2227d3118fe8741c
dataeng <viniciusdvale@gmail.com>
2022-10-04 09:27:05 -0300

    update to datacamp_dag

--- dags/datacamp_dag.py
+++ dags/datacamp_dag.py
@@ -1,6 +1,7 @@
 from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
 from airflow.operators.bash_operator import BashOperator
+import time
 
 default_args = {
     'start_date': days_ago(1),
@@ -13,5 +14,17 @@ default_args = {
 def etl_dag():
     rand_number = BashOperator(task_id='rand_number',bash_command='echo $RANDOM')
     echo_ex = BashOperator(task_id='echo_ex',bash_command='echo "Exemple!"')
-    rand_number
+
+    @task
+    def printme():
+        print("This goes in the logs!")
+
+    @task
+    def sleep(lenght_time):
+        time.sleep(lenght_time)
+
+
+    rand_number >> echo_ex
+    rand_number >> printme()
+    [echo_ex,printme()] >> sleep
 dag = etl_dag()
