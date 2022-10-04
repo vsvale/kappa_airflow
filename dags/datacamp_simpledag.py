be51e0149072b0bee76683485ccc35510954b9c9
dataeng <viniciusdvale@gmail.com>
2022-10-04 17:45:50 -0300

    simpler dag to test default args

--- dags/datacamp_simpledag.py
+++ dags/datacamp_simpledag.py
@@ -7,6 +7,18 @@ from datetime import timedelta
 from airflow.operators.python import BranchPythonOperator
 from airflow.operators.empty import EmptyOperator
 
+default_args = {
+    'start_date': days_ago(1),
+    'owner':'vale',
+    'email':'viniciusdvale@gmail.com',
+    'email_on_failure':False,
+    'email_on_retry:': False,
+    'email_on_success':False,
+    'sla': timedelta(minutes=10)
+
+}
+
+
 @dag(schedule_interval=None, default_args=default_args, catchup=False, tags=['datacamp'])
 def datacamp_simple_dag():
     rand_number = BashOperator(task_id='rand_number',bash_command='echo $RANDOM')
