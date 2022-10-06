e73c448b83b7dc0e8807f3018062a31d9013fe2f
dataeng <viniciusdvale@gmail.com>
2022-10-06 13:26:51 -0300

    back to begin

--- dags/stack_etl_pipeline_email.py
+++ dags/stack_etl_pipeline_email.py
@@ -1,9 +1,8 @@
 from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
 from airflow.operators.email_operator import EmailOperator
-from airflow.operators.bash_operator import BashOperator
 
-path_temp_csv = "/var/lib/rancher/k3s/storage/staging.csv"
+path_temp_csv = "/tmp/staging.csv"
 email_failed = "viniciusdvale@gmail.com"
 
 default_args = {
@@ -55,30 +54,39 @@ def stack_etl_pipeline_email():
                 inner join titles
                 on titles.emp_no = emp.emp_no limit 100; """
                 ,engine)
-        return df.values.tolist()
-
+        df.to_csv(path_temp_csv, index=False)
 
     @task
-    def transform(df_list):
+    def transform():
         import pandas as pd
-        df = df = pd.DataFrame (df_list, columns = ['emp_no', 'first_name','last_name','salary','title'])
+
+        df = pd.read_csv(path_temp_csv)
         df['name'] = df['first_name']+" "+df['last_name']
         print(df.head(5))
         df.drop(['emp_no','first_name','last_name'],axis=1,inplace=True)
-        return df
+        df.to_csv(path_temp_csv, index=False)
 
     @task
-    def load(df):
+    def load():
         import psycopg2
         import sqlalchemy
         import pandas as pd
 
         engine = sqlalchemy.create_engine('postgres+psycopg2://plumber:PlumberSDE@172.18.0.3:5433/dw_employee')
+        df = pd.read_csv(path_temp_csv)
         df.to_sql("d_employees",engine,if_exists="replace",index=False)
 
+    @task
+    def clean():
+        import os
+
+        try:
+            os.remove(path_temp_csv)
+        except OSError as e:
+            print ("Error: %s - %s." % (e.filename, e.strerror))
 
     email_task = EmailOperator(task_id='Notify', to=email_failed, subject='Stack pipeline first finalizado com sucesso', html_content='<p>Salvo em d_employees<p>')
 
-    load(transform(extract()))>>email_task
+    extract()>>transform()>>load()>>clean()>>email_task
 
 dag = stack_etl_pipeline_email()
