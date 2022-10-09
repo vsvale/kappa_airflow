13da60b2bb0a2faf0d7ef9161011ef60eec03134
dataeng <viniciusdvale@gmail.com>
2022-10-09 20:26:58 -0300

    test.sh

--- dags/stack/stack_templates.py
+++ dags/stack/stack_templates.py
@@ -14,6 +14,7 @@ description = "A DAG exemplo de template"
 def stack_templates():
     dt_exec = BashOperator(task_id='dt_exec', bash_command="echo data de execucao: {{ execution_date }}")
     dt_strf = BashOperator(task_id='dt_strf', bash_command="echo data de execucao formatada: {{ execution_date.strftime('%d/%m/%Y') }}")
+    script = BashOperator(task_id='script', bash_command="/scripts/test.sh", env={'execution_date':'{{ ds }}'})
 
-    dt_exec>>dt_strf
+    dt_exec>>dt_strf>>script
 dag = stack_templates()
