323cf7f5bb121cc0117d4804c32ef138feb8f392
dataeng <viniciusdvale@gmail.com>
2022-10-09 20:11:04 -0300

    template

---
+++ dags/stack/stack_templates.py
@@ -0,0 +1,19 @@
+from airflow.decorators import dag, task
+from airflow.utils.dates import days_ago
+from datetime import datetime, timedelta
+from airflow.operators.bash_operator import BashOperator
+
+default_args = {
+    'start_date': days_ago(1),
+    'end_date': datetime(2022,10,30)
+}
+
+description = "A DAG exemplo de template"
+
+@dag(schedule='@daily', default_args=default_args, catchup=False, tags=['stack', 'template'], description = description)
+def stack_templates():
+    dt_exec = BashOperator(task_id='dt_exec', bash_command="echo data de execucao: {{ execution_date }}")
+    dt_strf = BashOperator(task_id='dt_strf', bash_command="echo data de execucao formatada: {{ execution_date.strftime('%d/%m/%Y') }}")
+
+    dt_exec>>dt_strf
+dag = stack_templates()
