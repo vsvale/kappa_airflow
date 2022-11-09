2a048efc6ecefe61594d50ab3ff621bb24ca5ecb
dataeng <viniciusdvale@gmail.com>
2022-11-08 21:56:24 -0300

    task group

--- dags/example/bronze/example-customer-bronze.py
+++ dags/example/bronze/example-customer-bronze.py
@@ -1,6 +1,6 @@
 # imports
 from datetime import datetime, timedelta
-from airflow.decorators import dag
+from airflow.decorators import dag,task_group
 from airflow.utils.dates import days_ago
 from os import getenv
 from airflow.providers.amazon.aws.sensors.s3_key import S3KeySensor
@@ -28,49 +28,51 @@ description = "DAG in charge of training ml models"
 @dag(schedule='@daily', default_args=default_args,catchup=False,
 tags=['example','spark','customer','s3','sensor','k8s'],description=description)
 
-def example_customer_bronze():
-    # verify if new data has arrived on landing bucket
-    verify_customer_landing = S3KeySensor(
-    task_id='t_verify_customer_landing',
-    bucket_name=LANDING_ZONE,
-    bucket_key='example/src-example-customer/*/*.parquet',
-    wildcard_match=True,
-    timeout=18 * 60 * 60,
-    poke_interval=120,
-    aws_conn_id='minio')
+def example_bronze():
+    @task_group()
+    def example_customer_bronze():
+        # verify if new data has arrived on landing bucket
+        verify_customer_landing = S3KeySensor(
+        task_id='t_verify_customer_landing',
+        bucket_name=LANDING_ZONE,
+        bucket_key='example/src-example-customer/*/*.parquet',
+        wildcard_match=True,
+        timeout=18 * 60 * 60,
+        poke_interval=120,
+        aws_conn_id='minio')
 
-    # use spark-on-k8s to operate against the data
-    bronze_customer_spark_operator = SparkKubernetesOperator(
-    task_id='t_bronze_customer_spark_operator',
-    namespace='processing',
-    application_file='example-customer-bronze.yaml',
-    kubernetes_conn_id='kubeconnect',
-    do_xcom_push=True)
+        # use spark-on-k8s to operate against the data
+        bronze_customer_spark_operator = SparkKubernetesOperator(
+        task_id='t_bronze_customer_spark_operator',
+        namespace='processing',
+        application_file='example-customer-bronze.yaml',
+        kubernetes_conn_id='kubeconnect',
+        do_xcom_push=True)
 
-    # monitor spark application using sensor to determine the outcome of the task
-    monitor_bronze_customer_spark_operator = SparkKubernetesSensor(
-    task_id='t_monitor_bronze_customer_spark_operator',
-    namespace="processing",
-    application_name="{{ task_instance.xcom_pull(task_ids='t_bronze_customer_spark_operator')['metadata']['name'] }}",
-    kubernetes_conn_id="kubeconnect")
+        # monitor spark application using sensor to determine the outcome of the task
+        monitor_bronze_customer_spark_operator = SparkKubernetesSensor(
+        task_id='t_monitor_bronze_customer_spark_operator',
+        namespace="processing",
+        application_name="{{ task_instance.xcom_pull(task_ids='t_bronze_customer_spark_operator')['metadata']['name'] }}",
+        kubernetes_conn_id="kubeconnect")
 
-    # verify count source and destination
-    bronze_customer_spark_operator_verify = SparkKubernetesOperator(
-    task_id='t_bronze_customer_spark_operator_verify',
-    namespace='processing',
-    application_file='example-customer-bronze-verify.yaml',
-    kubernetes_conn_id='kubeconnect',
-    do_xcom_push=True)
+        # verify count source and destination
+        bronze_customer_spark_operator_verify = SparkKubernetesOperator(
+        task_id='t_bronze_customer_spark_operator_verify',
+        namespace='processing',
+        application_file='example-customer-bronze-verify.yaml',
+        kubernetes_conn_id='kubeconnect',
+        do_xcom_push=True)
 
-    # monitor spark application using sensor to determine the outcome of the task
-    monitor_bronze_customer_spark_operator_verify = SparkKubernetesSensor(
-    task_id='t_monitor_bronze_customer_spark_operator_verify',
-    namespace="processing",
-    application_name="{{ task_instance.xcom_pull(task_ids='t_bronze_customer_spark_operator_verify')['metadata']['name'] }}",
-    kubernetes_conn_id="kubeconnect")
+        # monitor spark application using sensor to determine the outcome of the task
+        monitor_bronze_customer_spark_operator_verify = SparkKubernetesSensor(
+        task_id='t_monitor_bronze_customer_spark_operator_verify',
+        namespace="processing",
+        application_name="{{ task_instance.xcom_pull(task_ids='t_bronze_customer_spark_operator_verify')['metadata']['name'] }}",
+        kubernetes_conn_id="kubeconnect")
 
-    # Confirm files are created
-    list_bronze_example_customer_folder = S3ListOperator(
+        # Confirm files are created
+        list_bronze_example_customer_folder = S3ListOperator(
         task_id='t_list_bronze_example_customer_folder',
         bucket=LAKEHOUSE,
         prefix='bronze/example/customer',
@@ -79,6 +81,7 @@ def example_customer_bronze():
         do_xcom_push=True)    
 
 
-    verify_customer_landing >> bronze_customer_spark_operator >> monitor_bronze_customer_spark_operator >> bronze_customer_spark_operator_verify
-    bronze_customer_spark_operator_verify >> monitor_bronze_customer_spark_operator_verify >> list_bronze_example_customer_folder
-dag = example_customer_bronze()
+        verify_customer_landing >> bronze_customer_spark_operator >> monitor_bronze_customer_spark_operator >> bronze_customer_spark_operator_verify
+        bronze_customer_spark_operator_verify >> monitor_bronze_customer_spark_operator_verify >> list_bronze_example_customer_folder
+    example_customer_bronze()
+dag = example_bronze()
