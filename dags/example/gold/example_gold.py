fb2b5297a9018b48170b0b56a582f267967e82b4
dataeng <viniciusdvale@gmail.com>
2022-11-22 17:17:31 -0300

    test ysql dimcurrency

--- dags/example/gold/example_gold.py
+++ dags/example/gold/example_gold.py
@@ -52,9 +52,36 @@ def example_gold():
         prefix='gold/example/dimcurrency',
         delimiter='/',
         aws_conn_id='minio',
-        do_xcom_push=True)    
+        do_xcom_push=True)
 
-        gold_dimcurrency_spark_operator >> monitor_gold_dimcurrency_spark_operator >> list_gold_example_dimcurrency_folder
+        # verify if new data has arrived on gold bucket
+        verify_dimcurrency_gold = S3KeySensor(
+        task_id='t_verify_dimcurrency_gold',
+        bucket_name=LAKEHOUSE,
+        bucket_key='gold/example/dimcurrency/*.parquet',
+        wildcard_match=True,
+        timeout=18 * 60 * 60,
+        poke_interval=120,
+        aws_conn_id='minio')
+
+        # use spark-on-k8s to operate against the data
+        ysql_dimcurrency_spark_operator = SparkKubernetesOperator(
+        task_id='t_ysql_dimcurrency_spark_operator',
+        namespace='processing',
+        application_file='example-dimcurrency-ysql.yaml',
+        kubernetes_conn_id='kubeconnect',
+        do_xcom_push=True)
+
+        # monitor spark application using sensor to determine the outcome of the task
+        monitor_ysql_dimcurrency_spark_operator = SparkKubernetesSensor(
+        task_id='t_monitor_ysql_dimcurrency_spark_operator',
+        namespace="processing",
+        application_name="{{ task_instance.xcom_pull(task_ids='dimcurrency_gold.t_gold_dimcurrency_spark_operator')['metadata']['name'] }}",
+        kubernetes_conn_id="kubeconnect")
+
+
+
+        gold_dimcurrency_spark_operator >> monitor_gold_dimcurrency_spark_operator >> list_gold_example_dimcurrency_folder >> verify_dimcurrency_gold >> ysql_dimcurrency_spark_operator >> monitor_ysql_dimcurrency_spark_operator
     dimcurrency_gold()
 
 dag = example_gold()
